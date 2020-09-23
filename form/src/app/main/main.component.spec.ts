6aa6d4b690bd26af2eb1c7afd9093b913e07d245
JoseLucasapp <josluc2016@gmail.com>
2020-09-23 12:34:24 -0300

    add new files

--- form/src/app/main/main.component.spec.ts
+++ form/src/app/main/main.component.spec.ts
@@ -1,20 +1,20 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { MainComponent } from './main.component';
+import { NovoProdutoComponent } from './novo-produto.component';
 
-describe('MainComponent', () => {
-  let component: MainComponent;
-  let fixture: ComponentFixture<MainComponent>;
+describe('NovoProdutoComponent', () => {
+  let component: NovoProdutoComponent;
+  let fixture: ComponentFixture<NovoProdutoComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ MainComponent ]
+      declarations: [ NovoProdutoComponent ]
     })
     .compileComponents();
   }));
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(MainComponent);
+    fixture = TestBed.createComponent(NovoProdutoComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
--- form/src/app/main/main.component.ts
+++ form/src/app/main/main.component.ts
@@ -1,15 +1,39 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { Router } from '@angular/router';
+import { DataService } from './../../data.service';
 
 @Component({
-  selector: 'app-main',
-  templateUrl: './main.component.html',
-  styleUrls: ['./main.component.sass']
+  selector: 'main',
+  templateUrl: './novo-produto.component.html',
+  styleUrls: ['./novo-produto.component.css']
 })
-export class MainComponent implements OnInit {
+export class NovoProdutoComponent implements OnInit {
 
-  constructor() { }
+  constructor(private service: DataService, private rota: Router) { }
+
+  novoProduto: any = {
+    name: null,
+    price: null,
+    sku: null
+  };
+  formSubmit = false;
 
   ngOnInit(): void {
+  }
+  onSubmit = (form)  => {
+    this.service.post(this.novoProduto).subscribe(sucess => this.alertarOk(), error => this.alertarErro());
+    setTimeout(this.onChangeRoute, 1000 * 3);
+    this.formSubmit = true;
+
+  }
+  onChangeRoute = () => {
+    this.rota.navigate(['/']);
+  }
+  alertarOk = () => {
+    alert(`O Produto ${this.novoProduto.name} com valor ${this.novoProduto.price.toLocaleString('pt-br', {style: 'currency', currency: 'BRL'})}. Foi criado com sucesso`);
+  }
+  alertarErro = () => {
+    alert('Produto Não Cadastrado');
   }
 
 }
