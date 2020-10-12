3fc6f28a46343ec8fc4e8d85c60faf5751442fae
JoseLucasapp <josluc2016@gmail.com>
2020-10-12 00:18:13 -0300

    add tkinter elements

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
 import json, requests
 from tkinter import *
 
+window = Tk()
+window.title('Buscar CEP')
 #cep logradouro complemento bairro localidade uf ddd
 cep = 58600000
 response = requests.get(f"https://viacep.com.br/ws/{cep}/json/")
 conteudo = json.loads(response.content)
+
+window.maxsize('500x500')
+window.minsize('500x500')
+window.geometry('500x500')
+window.mainloop()
