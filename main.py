053c1059682e62ba00c8e2830eed475fc71da470
JoseLucasapp <josluc2016@gmail.com>
2020-10-12 00:13:35 -0300

    add points to search

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import json, requests
 from tkinter import *
 
+#cep logradouro complemento bairro localidade uf ddd
 cep = 58600000
 response = requests.get(f"https://viacep.com.br/ws/{cep}/json/")
 conteudo = json.loads(response.content)
-print(conteudo)
