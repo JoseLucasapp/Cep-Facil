4f9431dbef74d29892e72af33974073c4cdf5862
JoseLucasapp <josluc2016@gmail.com>
2020-10-12 11:46:04 -0300

    update code and add new page

--- main.py
+++ main.py
@@ -1,8 +1,21 @@
 import json, requests
 from tkinter import *
+from tkinter import messagebox
+from tkinter import ttk
 
 window = Tk()
 window.title('Buscar CEP')
+
+tab_control = ttk.Notebook(window)
+cep = ttk.Frame(tab_control)
+endereco = ttk.Frame(tab_control)
+tab_control.add(cep,text = 'Buscar CEP')
+tab_control.add(endereco,text = 'Buscar Endereço')
+lbl1 = Label(cep, text='cep')
+lbl1.grid(column = 0, row=0)
+lbl2 = Label(endereco, text='endereco')
+lbl2.grid(column = 0, row=0)
+tab_control.pack(expand = 1, fill='both')
 
 def search_cep():
     uf = 'PB'
@@ -10,8 +23,11 @@ def search_cep():
     bairro = 'Frei Damião'
     response = requests.get(f"https://viacep.com.br/ws/{uf}/{cidade}/{bairro}/json/")
     conteudo = json.loads(response.content)
-    print(conteudo[0]['cep'])
-search_cep()
+    cep = conteudo[0]['cep']
+    messagebox.showinfo('CEP',cep)
+
+btn = Button(cep,text='Buscar CEP',command=search_cep)
+btn.grid(column = 2, row=1)
 
 window.maxsize(width = 500, height=500)
 window.minsize(width = 500, height=500)
