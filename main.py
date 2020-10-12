ce668b463987eaae36644f141c913d2ff40bede8
JoseLucasapp <josluc2016@gmail.com>
2020-10-12 00:10:48 -0300

    add requests commands

--- main.py
+++ main.py
@@ -1,1 +1,7 @@
 import json, requests
+from tkinter import *
+
+cep = 58600000
+response = requests.get(f"https://viacep.com.br/ws/{cep}/json/")
+conteudo = json.loads(response.content)
+print(conteudo)
