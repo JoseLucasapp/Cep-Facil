2beb1b016e1457ed823486065344ddc16f54e8ba
JoseLucasapp <josluc2016@gmail.com>
2020-10-13 21:49:12 -0300

    add items in window

--- main.py
+++ main.py
@@ -11,10 +11,6 @@ cep = ttk.Frame(tab_control)
 endereco = ttk.Frame(tab_control)
 tab_control.add(cep,text = 'Buscar CEP')
 tab_control.add(endereco,text = 'Buscar Endereço')
-lbl1 = Label(cep, text='cep')
-lbl1.grid(column = 0, row=0)
-lbl2 = Label(endereco, text='endereco')
-lbl2.grid(column = 0, row=0)
 tab_control.pack(expand = 1, fill='both')
 
 def search_cep():
@@ -23,10 +19,10 @@ def search_cep():
     bairro = 'Frei Damião'
     response = requests.get(f"https://viacep.com.br/ws/{uf}/{cidade}/{bairro}/json/")
     conteudo = json.loads(response.content)
-    cep = conteudo[0]['cep']
+    cep = 'Cep: ' + conteudo[0]['cep']
     messagebox.showinfo('CEP',cep)
 def search_endereco():
-    cep = '58600000'
+    cep = campo_cep.get()
     response = requests.get(f"https://viacep.com.br/ws/{cep}/json/")
     conteudo = json.loads(response.content)
     if conteudo['bairro']:
@@ -35,12 +31,15 @@ def search_endereco():
         endereco = conteudo['localidade'] +' / ' + conteudo['uf']
     messagebox.showinfo('Endereço', endereco)
 #----- Pagina 1 -----#
+
 btn1 = Button(cep,text='Buscar CEP',command=search_cep)
-btn1.grid(column = 2, row=1)
+btn1.place(x=0,y=0)
 
 #----- Pagina 2 -----#
+campo_cep = Entry(endereco)
+campo_cep.place(x=200,y=10)
 btn2 = Button(endereco, text='Buscar Endereço',command=search_endereco)
-btn2.grid(column = 2, row = 1)
+btn2.place(x=200,y=100)
 
 window.maxsize(width = 500, height=500)
 window.minsize(width = 500, height=500)
